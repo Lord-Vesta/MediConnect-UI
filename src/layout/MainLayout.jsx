9af3580774888757644e5c45ffc8150cc9ba0f4b
yash1302 <jadhavyash623@gmail.com>
2025-09-02 10:03:12 +0530

    PATCH : completed UI for chat screen

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,11 +4,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
+  const location = window.location.pathname;
+  console.log(location, "location");
   return (
     <div className="mx-4 sm:mx-[10%]">
       <Navbar />
       <Outlet />
-      <Footer />
+      {location !== "/chat" && <Footer />}
     </div>
   );
 };
